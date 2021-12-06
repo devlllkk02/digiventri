bc7a4b8b9bb45bb3fecd0567d489ae897b72d25e
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2021-12-07 01:45:07 +0530

    Ledger com added to app and create ledger.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Ledger from "./components/Ledger/Ledger";
 
 function App() {
   return(
     <Router>
       <Navbar/>
+      <Ledger/>
     </Router>
   );
 }
